b8d0feaa3dd83d7e04841ae8fb184cb87a8be3ac
sunny <sunny@sunnydeMacBook-Pro.local>
2018-05-30 23:27:59 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-SnapkitHidden_Tests/Pods-SnapkitHidden_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapkitHidden_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapkitHidden_Tests
+@end
---
+++ Example/Pods/Target Support Files/SnapkitHidden/SnapkitHidden-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SnapkitHidden : NSObject
+@end
+@implementation PodsDummy_SnapkitHidden
+@end
